ef91c745c368a7eed0e41d05f197678272c0b030
dharmiiiik <166748327+dharmiiiik@users.noreply.github.com>
2024-11-28 17:14:51 +0530

    Add files via upload

---
+++ Logic Questions/Que01.js
@@ -0,0 +1,142 @@
+// console.log((22 / 7) * 7 ** 2)
+
+// // // Que-2
+// let a = 10
+// let b = 20
+// let c = 5
+// console.log((a * b) / c + (b - a))
+
+// // Que-3
+// let x = 5
+// let y = 10
+
+// console.log((x + 2) * y)
+
+// // // Que-4 
+// // let num = 0
+// //  num+=15
+// //  console.log(num)
+// //  num-=5
+// //  console.log(num)
+// //  num*=3
+// //  console.log(num)
+// //  num/=2
+// //  console.log(num)
+
+
+// // // Que-5 
+// let score1 = 85
+// let score2 = 90
+
+// console.log(score1 > 80)
+// console.log(score2 > 80)
+
+// // Que-6 
+// let string1 = "apple"
+// let string2 = "banana"
+
+// console.log("string1" < "string2")
+
+
+// // Que-7 
+// let age = 25;
+// let hasDrivingLicense = true;
+
+// if (age >= 18 && hasDrivingLicense) {
+//     console.log("The person is eligible to drive.");
+// } else {
+//     console.log("The person is not eligible to drive.");
+// }
+
+// Que-8
+// let str = "123.45";
+// let num = parseFloat(str);  
+// let result = num + 10;      
+// console.log(result); 
+
+// Que-8a 
+// let number=22
+// if((number > 10 && number <50)|| number%5==0){
+//     console.log(number)
+// }
+// else{
+//     console("number is not >10 and <50")
+// }
+
+// Que-9
+// let value = true;
+// let result = String(value) + " is the value";
+// console.log(result);
+
+// Que-10 
+// let booleanValue = true
+// let result2 = booleanValue.tostring() + "is the value"
+// console.log(result2) 
+
+
+
+// // Que-11
+// let A=5
+// let B=10
+// console.log(A+B)
+
+
+// Que-12 
+
+// let number = 13
+
+// if (number > 0) {
+//     console.log("The number is positive.")
+// } else {
+//     console.log("The number is not positive.")
+// }
+
+
+// Que-14 
+// let temprature = 30
+// if (temprature > 25){
+//     console.log("It's a hot day ")
+// } else {
+//     console.log("it's normal day")
+// }
+
+
+// Que-15
+//  let number = 0
+//  if (number>=0){
+//     console.log("number is positive")
+//  }
+//  else{
+//     console.log("number is negative.")
+//  }
+
+// Que-16
+// let marks = 75
+
+// if (marks >= 90){
+//     console.log("Grade A")
+// }
+// else if (marks >=80 && marks<=89) {
+//     console.log("Grade B")
+// }
+// else if (marks >=70 && marks<=79 ){
+//     console.log(" Grade C ")
+// }
+// else {
+//     console.log("Grade D")
+// }
+
+// Que-17 
+
+// let number =16
+// if(number+=10 && number%2){
+//     console.log("number is both even and greater than 10")
+// }
+
+// Que-20
+let age=12
+if(age>=18 && age<=60){
+    console.log("you are  eligible")
+}else{
+    console.log("you are not eligible")
+}
